67c62e76bd7844c3cf4bbc7dad7452711e799267
mohongen <mo_hongen@uxsino.com>
2020-07-22 00:35:32 +0800

    ts使用接口限制函数

--- proj-second/src/second.ts
+++ proj-second/src/second.ts
@@ -103,3 +103,42 @@ function createSquare2(config: SquareConfig2): {color: string; area: number} {
 
 let mySquare3 = createSquare2({color: "black", opacity: 0.5})
 console.info(mySquare3)
+
+// * 函数类型
+// * 接口能够描述js中对象拥有的各种各样的外形。除了描述带有属性的普通对象外，
+// * 接口也可以描述函数类型
+
+// * 为了使用接口表示函数类型，我们需要给接口定义一个调用签名，他就像是
+// * 一个只有参数列表和返回值类型的函数定义。参数列表里的每个参数都需要名字和类型
+
+interface searchFunc {
+    (source: string, subString: string) : boolean;
+}
+
+// * 这样定义后，我们可以像使用其他接口一样使用这个函数类型的接口。
+
+let mySearch: searchFunc;
+mySearch = function(source: string, subString: string) {
+    let result = source.search(subString);
+    return result > -1;
+}
+
+// * 对于函数类型的类型检查来说，函数的参数名不需要与接口里定义的名字相匹配。如下
+let mySearch2: searchFunc;
+mySearch2 = function(sub: string, src: string): boolean {
+    let result = sub.search(src);
+    return result > -1
+}
+
+// * 由于函数的参数会逐个进行检查，要求对应位置上的参数类型是兼容的
+// * 如果不想指定类型，ts的类型系统会推断出参数类型，因为函数直接赋值给了SearchFunc类型变量
+// * 函数的返回值类型是通过其返回值推断出来的
+
+let mySearch3: searchFunc;
+mySearch3 = function(sub, src) {
+    let res = sub.search(src);
+    return res > -1
+}
+console.info(mySearch("10", "000101010"), mySearch2, mySearch3)
+
+
